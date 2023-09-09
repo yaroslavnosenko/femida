68cae1ace49b370a8004567ee8ee8f74e0e34f58
Yaroslav Nosenko <yaroslav.nosenko.mail@gmail.com>
2023-09-09 03:14:23 -0400

    parallax

---
+++ components/header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <header className="fixed w-screen z-10">
+      <div className="container">Header</div>
+    </header>
+  )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,10 @@ const config: Config = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
       },
     },
   },
